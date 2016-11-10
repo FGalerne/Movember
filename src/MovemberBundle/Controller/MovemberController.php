155d6ac4ca5d14eaa5a9de12009d89dae713eb18
Maxime-Coustes <maxime.coustes@hotmail.fr>
2016-11-10 07:28:00 +0100

    controller Movember

--- src/MovemberBundle/Controller/MovemberController.php
+++ src/MovemberBundle/Controller/MovemberController.php
@@ -121,4 +121,68 @@ class MovemberController extends Controller
             ->getForm()
         ;
     }
+
+
+/***************************************** code Ludo *********************************/
+
+    public function getAdresseAction(Request $request)
+    {
+        // google map geocode api url
+        $url = "http://maps.google.com/maps/api/geocode/json?address=chartres";
+
+// get the json response
+        $resp_json = file_get_contents($url);
+
+// decode the json
+        $resp = json_decode($resp_json, true);
+
+// response status will be 'OK', if able to geocode given address
+        if ($resp['status'] == 'OK') {
+
+            // get the important data
+            $lati = $resp['results'][0]['geometry']['location']['lat'];
+            $longi = $resp['results'][0]['geometry']['location']['lng'];
+            $formatted_address = $resp['results'][0]['formatted_address'];
+            //var_dump($lati);
+
+            echo $lati;
+            echo $longi;
+
+            $testMovember->setLat = $lati;
+            $testMovember->setLgt = $longi;
+
+            // verify if data is complete
+            if ($lati && $longi && $formatted_address) {
+
+                // put the data in the array
+                $data_arr = array();
+
+                array_push(
+                    $data_arr,
+                    $lati,
+                    $longi,
+                    $formatted_address
+                );
+
+                return $data_arr;
+
+            } else {
+                return false;
+            }
+
+        } else {
+            return false;
+
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($testMovember);
+        $em->flush();
+    }
+    /*   ********************************* fin code Ludo ************************/
+
+
+
+
+
 }
